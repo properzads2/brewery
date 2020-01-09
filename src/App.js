9a3e024df9aaa5a8cc6d3ca097e65361e3efab0b
epzads <epzads@gmail.com>
2020-01-09 15:12:04 -0500

    refactoring done and backend links added and tested

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import './App.css';
 import Breweri from './Components/Breweri'
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import CityList from './Components/CityList';
-import Detail from './Components/Detail'
 function App() {
   return (
       <div>
--- src/Components/Breweri.js
+++ src/Components/Breweri.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import CityList from './CityList';
 import Detail from './Detail'
 import Home from './Home'
-import { Switch, Route, Link } from 'react-router-dom';
+import { Switch, Route} from 'react-router-dom';
 
 export default class Breweri extends Component{
  
@@ -21,28 +21,43 @@ export default class Breweri extends Component{
 
     
 
-  // componentDidMount(){
-     
-     
-  //      let url = `'https://api.openbrewerydb.org/breweries?by_city=${this.state.city}'`
-  //      console.log(url) 
-  //      fetch(`https://api.openbrewerydb.org/breweries?by_city=${this.state.city}`,{
-  //   //  fetch(url,{
-  //       method: 'get',
-  //       headers: {'Content-Type':'application/json'}})
-  //       .then(response => response.json())
-  //       .then(data => {this.setState({brewery:data})})
-  //     //}
-  //  }
 
     ClickHandler =(name) =>{
       this.setState({city:name})
-      fetch(`https://api.openbrewerydb.org/breweries?by_city=${name}`,{
+       fetch(`https://api.openbrewerydb.org/breweries?by_city=${name}`,{
     
-         method: 'get',
-        headers: {'Content-Type':'application/json'}})
-      .then(response => response.json())
-      .then(data => {this.setState({brewery:data})}) 
+          method: 'get',
+         headers: {'Content-Type':'application/json'}})
+       .then(response => response.json())
+       .then(data => {this.setState({brewery:data})}) 
+    
+      
+      // Below Fetch Request is used for custom build api from ruby rails running on local port 3000
+      
+      // fetch('http://localhost:3000/brew', {
+      //   method: 'post',
+      //   headers: {'Content-Type':'application/json'},
+      //   body: JSON.stringify({
+      
+      //   "c":name
+      //   })}
+      //   )
+      //   .then((data) => {
+      //     console.log(data); 
+      //   });
+      
+        
+         
+      //   fetch(`http://localhost:3000/brew`,{
+    
+      //         method: 'get',
+      //        headers: {'Content-Type':'application/json'}})
+      //      .then(response => response.json())
+      //      .then(data => {this.setState({brewery:data})}) 
+        
+       
+    
+    
     }
 
    render(){
@@ -55,7 +70,7 @@ export default class Breweri extends Component{
         <Route path="/list/:city"  component={()=>{return <CityList list={this.state.brewery} city={this.state.city}/>}} />
         <Route path="/detail/:id"  component={(props)=>{
             let brewid = parseInt(props.match.params.id)
-            let selectedBrewery = this.state.brewery.find(brew => brew.id == brewid)
+            let selectedBrewery = this.state.brewery.find(brew => brew.id === brewid)
             return selectedBrewery ? <Detail brewer={selectedBrewery} city={this.state.city} /> : null }} />
       
       </Switch>
